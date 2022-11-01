fc463d0bdc2db48806766ec3a179bf8a0b54073f
Fahad Mohammad <fahad.mohammad06@gmail.com>
2022-11-01 02:48:22 -0400

    React colour picking game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [answers, setAnswers] = useState([""]);
+  const [wrongAnswer, setWrongAnswer] = useState(false);
+
+  const getRandomColor = () => {
+    const colors = [
+      "red",
+      "blue",
+      "green",
+      "pink",
+      "orange",
+      "black",
+      "yellow",
+      "purple",
+    ];
+    const generateColors = colors[Math.floor(Math.random() * colors.length)];
+    return generateColors;
+  };
+
+  useEffect(() => {
+    const actualColor = getRandomColor();
+    setColor(actualColor);
+    setAnswers(
+      [actualColor, getRandomColor(), getRandomColor()].sort(
+        () => 0.5 - Math.random()
+      )
+    );
+  }, []);
+
+  function handleAnswerClicked(answer) {
+    if (answer === color) {
+      setWrongAnswer(false);
+    } else {
+      setWrongAnswer(true);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="box" style={{ background: color }}></div>
+        <div className="buttons">
+          {answers.map((answer) => (
+            <button onClick={() => handleAnswerClicked(answer)} key={answer}>
+              {answer}
+            </button>
+          ))}
+          {wrongAnswer ? (
+            <div className="wrong">Wrong Answer</div>
+          ) : (
+            <div className="correct">Correct Answer</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
